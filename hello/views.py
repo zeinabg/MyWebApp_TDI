8cbec811d0b784ddf3147b2c8a179f17a61d73b5
Zeinab <z.gaeini@gmail.com>
2020-06-25 20:30:19 -0700

    add plot function

--- hello/views.py
+++ hello/views.py
@@ -26,19 +26,60 @@ def index(request):
 <form id='input' method='get' action='plot'>	
           <p>Ticker symbol: <input type='text' name='symbol' placeholder='GOOG' /></p>
           <p>
-          <input type="checkbox" name='features' value='close' />Closing price<br>
-          <input type="checkbox" name='features' value='adj_close' />Adjusted closing price<br>
           <input type="checkbox" name='features' value='open' />Opening price<br>
-          <input type="checkbox" name='features' value='adj_open' />Adjusted opening price<br>
+          <input type="checkbox" name='features' value='high' />High price<br>
+          <input type="checkbox" name='features' value='low' />Low price<br>
+          <input type="checkbox" name='features' value='close' />Closing price<br>
           </p>
           <p><input type="submit" value="Submit"></p>
           </form>
 		""")
 
+
+def plotTimeSeries (symbol,features):
+    
+    #recieve data by an API in pandas format
+    ts = TimeSeries(key='D5RXEJT6U9CLB7QR', output_format='pandas')
+    
+    def timeseries_symbol(symbol):
+        data, meta_data = ts.get_intraday(symbol=symbol , interval='60min', outputsize='full')
+        return data
+    data.reset_index(inplace= True)
+    
+    # prepare data for plot
+    x = data['date']
+    y0 = data['1. open']
+    y1 = data['2. high']
+    y2 = data['3. low']
+    y3 = data['4. close']
+
+    # output to static HTML file
+    output_file("Stock_timeseries.html")
+
+    # create a new plot
+    p = figure(
+       tools="pan,box_zoom,reset,save", title="Intraday Times Series for ... Stock",
+       x_axis_label='date', x_axis_type = 'datetime', plot_width=800
+    )
+
+    # add some renderers
+    if features[0]:
+        p.line(x, y0, legend_label="open", line_color="blue", line_dash="4 4")
+    if features[1]:
+        p.line(x, y1, legend_label="high", line_color="red")
+    if features[2]:
+        p.line(x, y2, legend_label="low", line_color="black")
+    if features[3]:
+        p.line(x, y2, legend_label="close", line_color="green")
+
+
+    # show the results
+    return show(p)
+
 def plot(request):
 	symbol = request.GET.get('symbol')
 	features = request.GET.getlist('features')
-	return HttpResponse("I have received your information: {} {}".format(str(type(request.GET)), features))
+	return HttpResponse(plotTimeSeries (symbol,features))
 
 
 def db(request):
