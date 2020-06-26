b20dd8f0d38066257613f7f39d53d13e41cd1b58
Zeinab <z.gaeini@gmail.com>
2020-06-25 19:31:21 -0700

    d

--- hello/views.py
+++ hello/views.py
@@ -19,9 +19,9 @@ from .models import Greeting
 
 def index(request):
 	# api_key = os.environ.get('API_KEY')
-	 input = request.POST
-	 if input:
-	 	return HttpResponse("I have received your information!")
+	 # input = request.POST
+	 # if input:
+	 # 	return HttpResponse("I have received your information!")
 	return HttpResponse("""
 <form id='input' method='post' action='index'>	
           <p>Ticker symbol: <input type='text' name='ticker' placeholder='GOOG' /></p>
